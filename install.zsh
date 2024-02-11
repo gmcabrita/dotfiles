781e30a4dab6d713b8760e4c5d017182e24d36eb
Gonçalo Cabrita <goncalo@mendescabrita.com>
2024-02-11 12:30:23 +0000

    Revert "use go from brew"
    This reverts commit 62115521c10536419c06a069a1be7188ecfff889.

--- install.zsh
+++ install.zsh
@@ -136,7 +136,7 @@ sudo defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 sudo sysctl kern.maxfiles=64000 kern.maxfilesperproc=28000
 
 brew install --cask 1password orbstack betterdisplay google-chrome linearmouse zed httpiee monodraw karabiner-elements db-browser-for-sqlite google-cloud-sdk spotify slack whatsapp telegram signal zoom raycast shureplus-motiv plex-media-server setapp macrorecorder sonos
-brew install --formula git gh sqlite fzf dos2unix colordiff bash asdf git-extras git-delta gnu-time jq fq less moreutils ncdu ripgrep grep rlwrap scc asciinema tree libpq cloud-sql-proxy glow gum mods vhs google-cloud-sdk gnu-tar gpg htop tailspin 1password-cli yt-dlp pgcli litecli hyperfine k9s rbspy vegeta go
+brew install --formula git gh sqlite fzf dos2unix colordiff bash asdf git-extras git-delta gnu-time jq fq less moreutils ncdu ripgrep grep rlwrap scc asciinema tree libpq cloud-sql-proxy glow gum mods vhs google-cloud-sdk gnu-tar gpg htop tailspin 1password-cli yt-dlp pgcli litecli hyperfine k9s rbspy vegeta
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono
 
@@ -154,6 +154,7 @@ asdf plugin-add python
 asdf plugin-add yarn
 asdf plugin-add pnpm
 asdf plugin-add zig
+adsf plugin-add golang
 asdf install nodejs $(asdf nodejs resolve lts --latest-available)
 asdf install bun latest
 asdf install ruby latest
@@ -161,9 +162,11 @@ asdf install python latest
 asdf install yarn 1.22.19
 asdf install pnpm latest
 asdf install zig latest
+asdf install golang latest
 asdf global nodejs $(asdf nodejs resolve lts --latest-available)
 asdf global bun latest
 asdf global ruby latest
 asdf global yarn 1.22.19
 asdf global pnpm latest
 asdf global zig latest
+asdf global golang latest
