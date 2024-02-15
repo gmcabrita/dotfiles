0c928a140f13dfcc62ad8ca406e79170a633bf8e
Gonçalo Cabrita <goncalo@mendescabrita.com>
2024-02-15 16:55:13 +0000

    tweak long running sql query

--- .psql/queries/long_running.sql
+++ .psql/queries/long_running.sql
@@ -5,7 +5,10 @@ select
     state,
     wait_event,
     wait_event_type,
-    pg_blocking_pids(pid)
+    pg_blocking_pids(pid),
+    usename,
+    application_name,
+    client_addr
 from pg_stat_activity
 where (now() - pg_stat_activity.xact_start) > interval '5 minutes'
 order by 2 desc
