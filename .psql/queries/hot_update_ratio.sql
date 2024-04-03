0bb2b55d3bb868e5d2ae5f73fa78eaddd695df4f
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2024-04-03 23:01:00 +0100

    Updates hot_update_ratio.sql

--- .psql/queries/hot_update_ratio.sql
+++ .psql/queries/hot_update_ratio.sql
@@ -1,11 +1,11 @@
 SELECT
   relname AS table_name,
-  seq_scan AS sequential_scans,
+  seq_scan AS seq_scans,
   idx_scan AS index_scans,
   n_tup_ins AS inserts,
   n_tup_upd AS updates,
-  n_tup_hot_upd AS hot_updates, (n_tup_hot_upd / n_tup_upd::float) * 100 as hot_update_ratio
-FROM
- pg_stat_user_tables
+  n_tup_hot_upd AS hot_updates,
+  ROUND((n_tup_hot_upd / n_tup_upd::numeric) * 100, 2) AS hot_update_pct
+FROM pg_stat_user_tables
 WHERE n_tup_upd > 0
 ORDER BY hot_updates DESC;
