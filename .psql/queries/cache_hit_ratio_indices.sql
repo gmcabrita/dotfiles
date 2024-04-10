1ef66daf01ac3b0a4e338bfe25f2c6d657a203a2
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2024-04-10 14:43:00 +0100

    Updates cache hit queries

--- .psql/queries/cache_hit_ratio_indices.sql
+++ .psql/queries/cache_hit_ratio_indices.sql
@@ -1,6 +1,9 @@
 SELECT
+  relname as table,
   sum(idx_blks_read) as idx_read,
   sum(idx_blks_hit)  as idx_hit,
-  (sum(idx_blks_hit) - sum(idx_blks_read)) / sum(idx_blks_hit) as ratio
-FROM
-  pg_statio_user_indexes;
+  ROUND((sum(idx_blks_hit) / NULLIF((sum(idx_blks_hit) + sum(idx_blks_read)), 0)::numeric) * 100, 2) as ratio
+FROM pg_statio_user_indexes
+WHERE idx_blks_read > 0
+GROUP BY relname
+ORDER BY ratio DESC;
--- .psql/queries/cache_hit_ratio_tables.sql
+++ .psql/queries/cache_hit_ratio_tables.sql
@@ -1,6 +1,9 @@
 SELECT
+  relname as table,
   sum(heap_blks_read) as heap_read,
   sum(heap_blks_hit)  as heap_hit,
-  sum(heap_blks_hit) / (sum(heap_blks_hit) + sum(heap_blks_read)) as ratio
-FROM
-  pg_statio_user_tables;
+  ROUND((sum(heap_blks_hit) / NULLIF((sum(heap_blks_hit) + sum(heap_blks_read)), 0)::numeric) * 100, 2) as ratio
+FROM pg_statio_user_tables
+WHERE heap_blks_read > 0
+GROUP BY relname
+ORDER BY ratio DESC;
