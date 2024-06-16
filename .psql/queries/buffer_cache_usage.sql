b0d84f0c5542c2d519adbe8a63a4f58077bbb5be
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2024-06-16 21:12:26 +0100

    buffer_cache_usage

---
+++ .psql/queries/buffer_cache_usage.sql
@@ -0,0 +1,8 @@
+with state as (
+  select count(*) filter (where relfilenode is not null) as used,
+         count(*) filter (where relfilenode is null) as empty,
+         count(*) as total
+    from pg_buffercache
+)
+select *, round(used * 1.0 / total * 100, 1) as percent
+  from state;
