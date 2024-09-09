193bc31d6044128cabf2bd520537a3e7bbf1f0e0
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2024-09-09 22:42:56 +0100

    add pg_crontab queries

---
+++ .psql/queries/connection_memory_breakdown.sql
@@ -0,0 +1,1 @@
+select pid, pg_size_pretty(rss*1000) as memory_overhead, fullcomm as command from pg_proctab() order by rss desc;
---
+++ .psql/queries/connection_total_memory_overhead.sql
@@ -0,0 +1,1 @@
+select pg_size_pretty(sum(rss)*1000) as total_memory_overhead from pg_proctab();
