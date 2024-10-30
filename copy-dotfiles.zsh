d78f708e5dacb8c0b909d0dc46be37817dfac4e4
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2024-10-30 12:55:13 +0000

    decouple copying of dotfiles

---
+++ copy-dotfiles.zsh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+cp .gitconfig $HOME/
+cp .gitignore $HOME/
+cp .githelpers $HOME/
+cp .zshrc $HOME/
+cp .default-gems $HOME/
+cp .psqlrc $HOME/
+cp .sqliterc $HOME/
+
+mkdir -p $HOME/Developer
+
+mkdir -p $HOME/.psql/queries
+cp .psql/queries/* $HOME/.psql/queries
+
+mkdir -p $HOME/.bin
+cp .bin/* $HOME/.bin
+
+mkdir -p $HOME/.raycast-scripts
+cp .raycast-scripts/* $HOME/.raycast-scripts
+
+mkdir -p $HOME/.config/ghostty
+cp .config/ghostty/* $HOME/.config/ghostty
+
+mkdir -p $HOME/.config/zed
+cp .config/zed/* $HOME/.config/zed
+
+mkdir -p $HOME/Library/Application\ Support/go/telemetry
+cp Library/Application\ Support/go/telemetry/* $HOME/Library/Application\ Support/go/telemetry
+
+mkdir -p $HOME/Library/Application\ Support/mods
+cp Library/Application\ Support/mods/* $HOME/Library/Application\ Support/mods
+
+mkdir -p $HOME/Library/Application\ Support/jj
+cp Library/Application\ Support/jj/* $HOME/Library/Application\ Support/jj
