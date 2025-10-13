559297cf7dd8258bf5e200a6144441188da81c1a
Gonçalo Mendes Cabrita <goncalo@mendescabrita.com>
2025-10-13 09:09:12 +0100

    update

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,5 @@
 if Version.match?(System.version(), ">= 1.18.0") do
   IEx.configure(auto_reload: true)
 end
+
+IEx.configure(inspect: [base: :decimal, charlists: :as_lists, binaries: :as_binaries])
