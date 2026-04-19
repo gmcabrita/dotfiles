32e52b74b31be2ceef526e977425ce935875fede
Gonçalo Mendes Cabrita <goncalo.mendes.cabrita@gmail.com>
2026-04-20 00:09:28 +0100

    update

---
+++ .raycast-scripts/zed-notes.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Zed Notes
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📒
+# @raycast.packageName Zed Notes
+
+set -e
+
+zed "/Users/gmcabrita/iCloud Drive/Notes"
